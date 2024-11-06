3d0d19060b53ce1996221ea5e2f1a6bf2af20265
V-Karthick <karthickveeramani.dev@gmail.com>
2024-11-06 21:13:32 +0530

    added camera functionalities for add favourite place page

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -1,6 +1,7 @@
 import { View, Text, ScrollView, TextInput, StyleSheet } from 'react-native'
 import React, { useState } from 'react'
 import { Colors } from '../../constants/colors'
+import ImagePicker from './ImagePicker'
 
 const PlaceForm = () => {
 
@@ -20,6 +21,7 @@ const PlaceForm = () => {
         value={enteredTitle}
         />
       </View>
+      <ImagePicker />
     </ScrollView>
   )
 }
