4dab4a9cdd40c1548e72977401918d8773bf390d
V-Karthick <karthickveeramani.dev@gmail.com>
2024-11-06 07:34:56 +0530

    Decided color themes and created input field

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -1,6 +1,7 @@
 import { View, Text, FlatList, StyleSheet } from 'react-native'
 import React from 'react'
 import PlaceItem from './PlaceItem'
+import { Colors } from '../../constants/colors'
 
 const PlacesList = ({places}) => {
     if(!places || places.length === 0)
@@ -30,6 +31,7 @@ const styles = StyleSheet.create({
         alignItems:"center"
     },
     fallbackText:{
-        fontSize:16
+        fontSize:16,
+        color:Colors.primary200
     }
 })
---
+++ constants/colors.js
@@ -0,0 +1,11 @@
+export const Colors={
+    primary50:"rgb(239, 177, 192)",
+    primary100:"rgb(236, 135, 160)",
+    primary200:"rgb(243, 118, 149)",
+    primary400:"rgb(241, 71, 114)",
+    primary500:"rgb(244, 13, 71)",
+    primary700:"rgb(205, 5, 55)",
+    primary800:"rgb(160, 5, 44)",
+    accent500:"rgb(255, 225, 1)",
+    gray700:"rgb(100, 96, 97)",
+}
